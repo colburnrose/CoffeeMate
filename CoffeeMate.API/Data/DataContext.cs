20df7c0b4edf08c4942e55b242f500f8d9038b04
colburnrose <colburnsanders@gmail.com>
2018-08-21 14:51:06 -0500

    Initial Commit

---
+++ CoffeeMate.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using CoffeeMate.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeMate.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
