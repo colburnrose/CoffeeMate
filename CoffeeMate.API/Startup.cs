6c8a67d8b951092bc99aa8e574021c4823b5e70e
colburnrose <colburnsanders@gmail.com>
2018-08-28 22:13:57 -0500

    Added authentication middleware and attributes.

--- CoffeeMate.API/Startup.cs
+++ CoffeeMate.API/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using CoffeeMate.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CoffeeMate.API
 {
@@ -32,6 +35,15 @@ namespace CoffeeMate.API
             services.AddCors();
             // injecting IAuth into controllers & gets implementation logic from AuthRepo.
             services.AddScoped<IAuthRepository, AuthRepository>(); // service is created once per request. one instance for each HTTP request.
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(o => {
+                o.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +61,9 @@ namespace CoffeeMate.API
             // app.UseHttpsRedirection();
             // CORS support
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseAuthentication();
             app.UseMvc();
+            
         }
     }
 }
